aa9939a8edd95242a9427d6438b24a455701f14a
TaskeenMirajannavar <140390197+TaskeenMirajannavar@users.noreply.github.com>
2023-09-03 23:12:21 +0530

    Add files via upload

---
+++ assing_fibonacci.py
@@ -0,0 +1,17 @@
+nterms = int(50)
+
+n1, n2 =0,1
+count =0 
+if nterms <= 0:
+    print("Please enter a positive number")
+elif nterms == 1:
+    print("Fibonnaci sequence upto",nterms,":")
+    print(n1)
+else:
+    print("Fibonnaci sequence:")
+    while count < nterms:
+        print(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        count += 1
